cad440c57c9343b731568ef97697c7e6acf2eae7
Your Name <shadburn.sean@gmail.com>
2016-12-13 15:32:09 -0500

    Third commit

---
+++ flashCards.js
@@ -0,0 +1,8 @@
+var BasicFlashCard = require('./basicFlashCard'); 
+var clozeFlashCard = require('./clozeFlashCard'); 
+var prompt = require('prompt'); 
+var fs = require('fs'); 
+
+prompt.start();
+
+prompt.get([])
