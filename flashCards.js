776c3b2942132095ac07ba4a4a79ab4bba69ec2c
Your Name <shadburn.sean@gmail.com>
2016-12-15 15:31:58 -0500

    Fourth commit

--- flashCards.js
+++ flashCards.js
@@ -1,8 +1,35 @@
 var BasicFlashCard = require('./basicFlashCard'); 
 var clozeFlashCard = require('./clozeFlashCard'); 
-var prompt = require('prompt'); 
+var inquirer = require('inquirer'); 
 var fs = require('fs'); 
 
-prompt.start();
 
-prompt.get([])
+
+function flashcard(){
+	inquirer.prompt([
+
+		{
+			type: "confirm",
+			message: "Are you ready for some flashcards?",
+			name: "confirmation"
+		},
+		
+		{
+			type: "checkbox",
+			message: "What is your experience level?  ",
+            choices: ("Beginner", "Intermediate", "Genius"), 
+			name: "boxofChecks"
+		},
+		
+		
+	]).then(function (user) {
+		
+
+		// If we log that user as a JSON, we can see how it looks.
+		console.log(JSON.stringify(user, null, 2));
+		confirmation = user.confirmation;
+		boxofChecks = user.boxofChecks;
+		
+
+	});
+}
